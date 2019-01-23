a4dce674af5902e1782f44e660d6877e509e6110
LittleEndianRoot <littleendianroot@gmail.com>
2019-01-23 14:26:31 +0000

    Adding the BinaryTutrial project to the repository

---
+++ BinaryTutorial/src/BinaryTutorial.cpp
@@ -0,0 +1,18 @@
+/*
+ * ============================================================================
+ * Name        : BinaryTutorial.cpp
+ * Author      : endian
+ * Description : This program will take your through the steps of how the binary
+ * 				 number system works, and I will show some easy way to calculate
+ * 				 and convert from different number systems.
+ *============================================================================
+ */
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	//Binary Tutorial coming soon
+	return 0;
+}
