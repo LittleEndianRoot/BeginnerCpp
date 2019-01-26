762c52cc7ccdd359c32a0c2939bc3fe629ded531
LittleEndianRoot <littleendianroot@gmail.com>
2019-01-26 22:10:39 +0000

    Edited the IntegerTypes.cpp file

--- IntegerTypes/src/IntegerTypes.cpp
+++ IntegerTypes/src/IntegerTypes.cpp
@@ -1,6 +1,6 @@
 /*
  * ============================================================================
- * Name        : BinaryTutorial.cpp
+ * Name        : IntegerTypes.cpp
  * Author      : endian
  * Description : This program will take you through all the different integer
  * 				 types and the meaning behind them.
@@ -8,7 +8,7 @@
  */
 
 #include <iostream>
-#include <limits>
+#include <climits>
 
 using namespace std;
 
@@ -16,16 +16,23 @@ int main() {
 
 	int value = 54656;
 	cout << value << endl;
+	cout << endl;
+
+	cout << "Maximum integer value: " << INT_MAX << endl;
+	cout << "Minimum integer value: " << INT_MIN << endl;
+	cout << endl;
 
 	long int lValue = 2345325345345;
 	cout << lValue << endl;
+	cout << endl;
 
 	short int sValue = 23434;
 	cout << sValue << endl;
+	cout << endl;
 
-	cout << "Size of int: " << sizeof(int) << endl;
-	cout << "Size of short int: " << sizeof(short int) << endl;
-	cout << "Size of long int: " << sizeof(long int ) << endl;
+	cout << "Size of int in bytes: " << sizeof(int) << endl;
+	cout << "Size of short int in bytes: " << sizeof(short int) << endl;
+	cout << "Size of long int int bytes: " << sizeof(long int ) << endl;
 
 
 
