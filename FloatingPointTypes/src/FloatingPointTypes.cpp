3a36e5b323c555d05d72a2000631f60a24e91df4
LittleEndianRoot <littleendianroot@gmail.com>
2019-01-27 23:53:52 +0000

    Adding a new project called FloatingPointTypes to the repository

---
+++ FloatingPointTypes/src/FloatingPointTypes.cpp
@@ -0,0 +1,20 @@
+/*
+ * ============================================================================
+ * Name        : FloatingPointTypes.cpp
+ * Author      : endian
+ * Description :
+ *============================================================================
+ */
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+
+	float fValue = 123.45f;
+	cout << fValue << endl;
+
+
+	return 0;
+}
