9853ddb031b5c0e3024963855bbd953fa2c6067c
LittleEndianRoot <littleendianroot@gmail.com>
2019-01-28 22:58:28 +0000

    Added a new branch called FloatingPointTypes, Also updated the FloatingPointTypes project

--- FloatingPointTypes/src/FloatingPointTypes.cpp
+++ FloatingPointTypes/src/FloatingPointTypes.cpp
@@ -2,7 +2,10 @@
  * ============================================================================
  * Name        : FloatingPointTypes.cpp
  * Author      : endian
- * Description :
+ * Description : This is a program to show you the different types of floating
+ * 				 point types. Or in other words types that have decimal points
+ * 				 in the number. We get to see the precision of the different
+ * 				 types of floating point numbers.
  *============================================================================
  */
 #include <iostream>
@@ -12,9 +15,20 @@ using namespace std;
 
 int main() {
 
-	float fValue = 123.45f;
-	cout << fValue << endl;
+	float fValue = 123.4512378f;
 
+	cout << "The size of float is: " << sizeof(float) << " bytes" << endl;
+	cout << setprecision(20) << fValue << endl;
+
+	double dValue = 123.4512378;
+
+	cout << "The size of double is: " << sizeof(double) << " bytes" << endl;
+	cout << setprecision(20) << dValue << endl;
+
+	long double lValue = 123.4512378;
+
+	cout << "The size of long double is: " << sizeof(long double) << " bytes" << endl;
+	cout << setprecision(20) << lValue << endl;
 
 	return 0;
 }
