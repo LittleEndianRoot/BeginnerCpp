b349b5c22530345ec73460058619dbab5cdb547f
LittleEndianRoot <littleendianroot@gmail.com>
2019-02-07 20:50:36 +0000

    Added a new branch called ForLoop

---
+++ ForLoop/src/ForLoop.cpp
@@ -0,0 +1,23 @@
+/*
+ * ============================================================================
+ * Name        : ForLoop.cpp
+ * Author      : endian
+ * Description :
+ * ============================================================================
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	for(int i=0; i<10; i++)
+	{
+		cout << "We have passed through the loop " << (i+1) << " times."<< endl;
+	}
+
+	cout << "\nFor loop finished!" << endl;
+
+	return 0;
+}
