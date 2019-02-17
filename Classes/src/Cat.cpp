d4b1841bd849ce710eb2d8070a3eb7b4e8c0f8fa
LittleEndianRoot <littleendianroot@gmail.com>
2019-02-17 14:41:09 +0000

    Adding a tutorial called Classes

---
+++ Classes/src/Cat.cpp
@@ -0,0 +1,22 @@
+/*
+ * Cat.cpp
+ *
+ *  Created on: 17 Feb 2019
+ *      Author: endian
+ */
+#include <iostream>
+#include "Cat.h"
+
+using namespace std;
+
+void Cat::speak()
+{
+	cout << "Meow!" << endl;
+}
+
+void Cat::jump()
+{
+	cout << "Jumping to top of bookcase" << endl;
+}
+
+
